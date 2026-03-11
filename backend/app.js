6fe891ba053d03c8fa1b53bdb5e618d6265a22c8
V4Vikaskumargit config --global user.email <vikaskumar77103@gmail.com>
2026-03-11 16:19:18 +0530

    cors origin

--- backend/app.js
+++ backend/app.js
@@ -26,7 +26,7 @@ app.use(express.urlencoded({extended : true}));
 // axios ki request ko chalane ke liye;
 app.use(express.json());
 app.use(cors({
-    origin : env.CROSS_ORIGIN,
+    origin : env.CROSS_ORIGIN || '*',
     credentials : true
 }));
 const io = new Server(httpServer, {
